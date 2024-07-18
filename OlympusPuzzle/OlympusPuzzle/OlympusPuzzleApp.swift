4c772b7764d2118c71152ba2ce824f92ab221a5c
Kirill Manuilenko <m.kirill4163@gmail.com>
2024-07-18 17:32:51 +0300

    Initial commit

---
+++ OlympusPuzzle/OlympusPuzzle/OlympusPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlympusPuzzleApp.swift
+//  OlympusPuzzle
+//
+//  Created by Kirill Manuilenko on 18.07.24.
+//
+
+import SwiftUI
+
+@main
+struct OlympusPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
